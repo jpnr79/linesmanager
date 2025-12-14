f62cb8ff1cb8f66afb510549a0587a6fea09e4ca
Pedro Rocha <jpnr79@gmail.com>
2025-12-14 20:23:11 +0000

    refactor: remove unnecessary ALTER TABLE statement from SQL schema

--- sql/0.5.0.sql
+++ sql/0.5.0.sql
@@ -1,3 +1,3 @@
 INSERT INTO `glpi_plugin_linesmanager_configs` (type, value) VALUES ('fill_line_information', '0') ON DUPLICATE KEY UPDATE value = value;
 
-ALTER TABLE `glpi_plugin_linesmanager_forwards`  ADD `typeforward` VARCHAR(20) NOT NULL DEFAULT 'all' COMMENT 'all, busy, no answer'  AFTER `numplan`;
+
